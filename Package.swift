3f06c3124442be203d95a3d2f1ae858aa5f65e13
Sananhusain <mac5.nogiz@gmail.com>
2026-06-29 11:26:03 +0530

    Release version 0.1.8

--- Package.swift
+++ Package.swift
@@ -1,17 +1,30 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
     name: "snapcut",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v16)
     ],
     products: [
         .library(
             name: "snapcut",
-            targets: ["snapcut"]),
+            targets: ["snapcutWrapper"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Giphy/giphy-ios-sdk", exact: "2.3.2"),
+        .package(url: "https://github.com/SDWebImage/libwebp-Xcode", exact: "1.5.0")
     ],
     targets: [
+        .target(
+            name: "snapcutWrapper",
+            dependencies: [
+                .target(name: "snapcut"),
+                .product(name: "GiphyUISDK", package: "giphy-ios-sdk"),
+                .product(name: "libwebp", package: "libwebp-Xcode")
+            ],
+            path: "Sources/snapcutWrapper"
+        ),
         .binaryTarget(
             name: "snapcut",
             path: "snapcut.xcframework"
