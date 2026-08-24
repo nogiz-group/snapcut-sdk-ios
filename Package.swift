9f20ace2b970abeb74915e773cd4b4d95eec47a1
Priyanka <priyanka@nogiz.com>
2026-08-24 16:12:24 +0530

    Release version 0.3.41

--- Package.swift
+++ Package.swift
@@ -13,9 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Giphy/giphy-ios-sdk", exact: "2.3.2"),
-        .package(url: "https://github.com/SDWebImage/libwebp-Xcode", exact: "1.5.0"),
-        .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.0"),
-        .package(url: "https://github.com/ggerganov/whisper.spm", exact: "1.6.2")
+        .package(url: "https://github.com/SDWebImage/libwebp-Xcode", exact: "1.5.0")
     ],
     targets: [
         .target(
@@ -23,9 +21,7 @@ let package = Package(
             dependencies: [
                 .target(name: "snapcut"),
                 .product(name: "GiphyUISDK", package: "giphy-ios-sdk"),
-                .product(name: "libwebp", package: "libwebp-Xcode"),
-                .product(name: "ZIPFoundation", package: "ZIPFoundation"),
-                .product(name: "whisper", package: "whisper.spm")
+                .product(name: "libwebp", package: "libwebp-Xcode")
             ],
             path: "Sources/snapcutWrapper"
         ),
